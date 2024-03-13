8dd16bb56e9bf88b27174949872750279c9675b0
pedro <phedroclooney@gmail.com>
2024-03-12 23:20:35 -0300

    nova versao

--- config/database.php
+++ config/database.php
@@ -86,11 +86,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'database-1.chggya42a5ow.us-east-2.rds.amazonaws.com'),//127.0.0.1
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'database-1')//forge,
+            'username' => env('DB_USERNAME', 'admin'),//forge
+            'password' => env('DB_PASSWORD', 'pedro0000'),//''
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
