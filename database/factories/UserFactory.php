93baeea046e52b87bcd9021153d6f3da804b3ae8
Pedro <phedroclooney@gmail.com>
2025-01-30 01:30:30 -0300

    AM-2024-12 - Fix automated test errors

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'tenant_id' => SimpleTenantDatabase::first()->id ?: factory(SimpleTenantDatabase::class)->create()->id,
+        'tenant_id' => SimpleTenantDatabase::first() ? SimpleTenantDatabase::first()->id : factory(SimpleTenantDatabase::class)->create()->id,
         'active' => 'yes'
     ];
 });
