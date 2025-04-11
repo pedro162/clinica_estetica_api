e5e1f5ccdd305d2d0cb94692eef6281877b77f50
Pedro Aguiar <60150082+pedro162@users.noreply.github.com>
2025-04-11 14:18:47 -0300

    Update entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -9,14 +9,6 @@ echo "📦 Preparando a aplicação Laravel..."
 # Instala as dependências (se ainda não tiver sido feito no Dockerfile)
 # composer install --no-interaction --prefer-dist --optimize-autoloader
 
-# Gera a APP_KEY se não estiver definida
-if ! grep -q "APP_KEY=" .env || grep -q "APP_KEY=$" .env; then
-  echo "🔐 Gerando APP_KEY..."
-  php artisan key:generate
-else
-  echo "🔐 APP_KEY já existe."
-fi
-
 # Cache de configs
 php artisan config:clear
 php artisan config:cache
