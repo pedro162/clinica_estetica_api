275bcc9549dd7e5200dae16ced57ffb7443fb370
Pedro <phedroclooney@gmail.com>
2025-08-16 20:49:26 -0300

    Adjusting the project for laravel 11 version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::unguard(); // opcional
+        Schema::defaultStringLength(191);
+
+        // Para manter o namespace antigo
+        Model::preventLazyLoading(!app()->isProduction());
     }
 }
